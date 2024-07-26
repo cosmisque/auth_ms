b9cfd0b8a3388721613f3c99c953ff96f15282e7
cosmisque <cjyclx@gmail.com>
2024-07-27 00:26:49 +0800

    refactor: replace async handler to library

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import AuthService from '../service/authService';
 import { loginSchema, registrationSchema } from '../validation/authValidation';
 import { parse } from 'cookie';
-import asyncHandler from '../utils/asyncHandler';
+import asyncHandler from 'express-async-handler';
 
 class AuthController {
   private authService: AuthService;
@@ -11,47 +11,54 @@ class AuthController {
     this.authService = new AuthService();
   }
 
-  login = asyncHandler(async (req: Request, res: Response): Promise<Response> => {
+  login = asyncHandler(async (req: Request, res: Response) => {
     const userData = loginSchema.parse(req.body);
     const result = await this.authService.login(userData);
 
     if (!result) {
-      return res.status(400).json({ error: 'User not found' });
+      res.status(400).json({ error: 'User not found' });
+      return;
     }
     const { refreshToken } = result;
-    return res
+    res
       .cookie('rft', refreshToken, {
         httpOnly: true,
         sameSite: 'strict'
       })
       .status(201)
       .json(result);
+    return;
   });
 
-  register = asyncHandler(async (req: Request, res: Response): Promise<Response> => {
+  register = asyncHandler(async (req: Request, res: Response) => {
     const userData = registrationSchema.parse(req.body);
     const user = await this.authService.register(userData);
 
     if (!user) {
-      return res.status(400).json({ error: 'User not found' });
+      res.status(400).json({ error: 'User not found' });
+      return;
     }
-    return res.status(201).json(user);
+    res.status(201).json(user);
+    return;
   });
 
-  accessToken = asyncHandler(async (req: Request, res: Response): Promise<Response> => {
+  accessToken = asyncHandler(async (req: Request, res: Response) => {
     const cookie = parse(req.headers.cookie || '');
     const refreshToken = cookie.rft;
 
     if (!refreshToken) {
-      return res.status(401).json({ error: 'Not Authenticated' });
+      res.status(401).json({ error: 'Not Authenticated' });
+      return;
     }
     const accessToken = await this.authService.getAccessWithRefresh(refreshToken);
 
     if (!accessToken) {
-      return res.status(401).json({ error: 'Not Authenticated' });
+      res.status(401).json({ error: 'Not Authenticated' });
+      return;
     }
 
-    return res.status(201).json(accessToken);
+    res.status(201).json(accessToken);
+    return;
   });
 }
 
